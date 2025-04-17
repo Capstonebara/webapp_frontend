bb8d456449a22822f62854eca32f5d7600877056
thainq <thainguyen12.work@gmail.com>
2025-04-17 22:52:37 +0700

    fix router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,21 @@
 
 import { Spinner } from "@/components/ui/spinner";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
 
-  const storedValue = localStorage.getItem("accessToken");
-  const username = localStorage.getItem("user");
+  useEffect(() => {
+    const storedValue = localStorage.getItem("accessToken");
+    const username = localStorage.getItem("user");
 
-  if (storedValue && username) {
-    router.push("/dashboard");
-  } else {
-    router.push("/auth");
-  }
+    if (storedValue && username) {
+      router.push("/dashboard");
+    } else {
+      router.push("/auth");
+    }
+  }, [router]);
 
   return (
     <div className="flex items-center justify-center h-screen">
