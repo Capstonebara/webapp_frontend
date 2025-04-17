06a24de3ca9bb27b343daec76fa2a6bd2b0122cf
thainq <thainguyen12.work@gmail.com>
2025-04-17 22:45:53 +0700

    add router

--- src/app/dashboard/dashboard-sidebar.tsx
+++ src/app/dashboard/dashboard-sidebar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { Users, Clock, LogOut, LayoutDashboard } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   Sidebar,
   SidebarContent,
